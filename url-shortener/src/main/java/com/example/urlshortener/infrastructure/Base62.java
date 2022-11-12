fc8353cc1574e462d42b2091c5d465f4f722270f
eunyoung23 <eun970923@naver.com>
2022-11-12 18:28:53 +0900

    [Fix] Base62클래스에 bit.ly 붙이는 문자열 삭제

--- url-shortener/src/main/java/com/example/urlshortener/infrastructure/Base62.java
+++ url-shortener/src/main/java/com/example/urlshortener/infrastructure/Base62.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Base62 {
 
-    private final String URL_PREFIX="bit.ly/";
     private final int BASE62=62;
     private final String BASE62_CHAR="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -15,7 +14,7 @@ public class Base62 {
             sb.append(BASE62_CHAR.charAt((int) (param%BASE62)));
             param/=BASE62;
         }
-        return URL_PREFIX+sb.toString();
+        return sb.toString();
     }
 
 
